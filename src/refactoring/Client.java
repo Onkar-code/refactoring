599bb34683ee24c4bdca98d49ca82a9b52c2e8eb
onkar-code <cf19onkar.singh@iesjoandaustria.org>
2020-10-28 20:04:02 +0100

    Fiveth version

--- src/refactoring/Client.java
+++ src/refactoring/Client.java
@@ -85,4 +85,35 @@ public class Client {
 	            "Punts guanyats: " + bonificacionsTotals() + "\n";
 		
 	}
+	
+	public String informeHTML() {
+    	return composaCapçaleraHTML() + composaDetallsHTML() + composaPeuHTML();
+    }
+	
+	public String composaCapçaleraHTML() {
+		return "<h1>Informe de lloguers</h1>" +
+				"<p>Informe de lloguers del client <em>" + getNom() + "</em> (<strong>" + getNif() + "</strong>)</p>";
+    }
+	
+	public String composaDetallsHTML() {
+    	String resultat = "<table>" +
+    	"<tr><td><strong>Marca</strong></td>"
+    	+ "<td><strong>Model</strong></td>"
+    	+ "<td><strong>Import</strong></td></tr>";  
+    	for (Lloguer lloguer: lloguers) {
+          	
+            // composa els resultats d'aquest lloguer
+            resultat += "<tr><td>" +
+                lloguer.getVehicle().getMarca() + "</td><td>" +
+                lloguer.getVehicle().getModel() + "</td><td>"  +
+                (lloguer.quantitat() * 30) + "€" + "</td><td>";            
+        }
+    	return resultat + "</table>";    	
+    }
+    
+	public String composaPeuHTML() {		
+		return "<p>Import a pagar: <em>" + importeTotal() + "€</em></p>" +
+				"<p>Punts guanyats: <em>" + bonificacionsTotals() + "</em></p>";
+		}
+    
 }
